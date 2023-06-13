135e1175b62cda1f277c061b026531e64c9321d9
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2023-06-13 16:56:55 -0700

    Removed separated html files since I couldn't get external js file to work with external html files

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,11 @@
-$(document).ready(function(){
-    const yearSpan = document.getElementById("currentYear");
-    console.log("document", document)
-    console.log("year", yearSpan)
-    
-    const setCurrentYear = () => {
-        let currentDate = new Date();
-        let currentYear = currentDate.getFullYear();
-        yearSpan.innerHTML = currentYear;
-    }
+const yearSpan = document.getElementById("currentYear");
+console.log("document", document)
+console.log("year", yearSpan)
 
-    setCurrentYear();
-});
+const setCurrentYear = () => {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    yearSpan.innerHTML = currentYear;
+}
+
+setCurrentYear();
