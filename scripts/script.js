dfe7e8f2abfc7f839804b8da088033a0057ce98b
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2023-06-13 16:48:59 -0700

    Moved some scripts around to get copyright section working

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
-const yearSpan = document.querySelector("span#currentYear");
+$(document).ready(function(){
+    const yearSpan = document.getElementById("currentYear");
+    console.log("document", document)
+    console.log("year", yearSpan)
+    
+    const setCurrentYear = () => {
+        let currentDate = new Date();
+        let currentYear = currentDate.getFullYear();
+        yearSpan.innerHTML = currentYear;
+    }
 
-const setCurrentYear = () => {
-    let currentDate = new Date();
-    let currentYear = currentDate.getFullYear();
-    yearSpan.innerHTML = currentYear;
-}
-
-setCurrentYear();
+    setCurrentYear();
+});
