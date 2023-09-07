ed318a26c0b58188e3ae955898b347bbdc7750cc
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2023-09-07 15:08:41 -0700

    Added transitions to hamburger menu opening and closing

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 const yearSpan = document.getElementById("currentYear");
+const hamburgerIcon = document.getElementsByClassName("hamburgerIcon")[0];
+const hamburgerLines = hamburgerIcon.getElementsByClassName("line");
 
 const setCurrentYear = () => {
     let currentDate = new Date();
     let currentYear = currentDate.getFullYear();
     yearSpan.innerHTML = currentYear;
 }
+
+hamburgerIcon.addEventListener("click", () => {
+    console.log("clicked")
+    hamburgerIcon.classList.toggle("menuOpened")
+    hamburgerIcon.classList.toggle("menuClosed")
+})
 
 setCurrentYear();
